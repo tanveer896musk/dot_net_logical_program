bfc5265390f77314993e77d7187b98332f1f42f5
Tanveer Sayyed <tanveersayyed1212@gmail.com>
2025-04-10 13:38:33 +0530

    commited by tanveer

---
+++ Armstrong.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logical_program
+{
+    public class Armstrong
+    {
+        public static void armstrongnumber(String[] args)
+        {
+
+        }
+    }
+}
